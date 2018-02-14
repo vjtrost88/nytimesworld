8aa43a50a90c0ef5ff50f9a95723aea6a6add248
Vince Trost <vincentjosephtrost@gmail.com>
2018-02-14 04:09:04 -0500

    update

--- dev/vgg16.py
+++ dev/vgg16.py
@@ -2,6 +2,7 @@ from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
+import pandas as pd
 
 model = VGG16(weights='imagenet', include_top=False)
 
