6ea6a5eed2dbbfd3be04bb929984749fea5af0ec
Vince Trost <vincent.trost@yahoo.com>
2018-02-14 15:29:59 -0500

    update

--- dev/vgg16.py
+++ dev/vgg16.py
@@ -1,25 +1,30 @@
+import numpy as np
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
-import numpy as np
-import pandas as pd
 
-model = VGG16(weights='imagenet', include_top=False)
 
-f = open("file-paths.txt", "r")
-contents = f.readlines()
-contents = [line.rstrip('\n') for line in contents]
-
-features = []
-
-for img_path in contents:
-
+def extract_features_with_vgg16(model, img_path):
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
+    features = model.predict(x).flatten()
+    return features
 
-    features.append(model.predict(x))
+def main():
+    f = open("file-paths-r10.txt", "r")
+    contents = f.readlines()
+    contents = [line.rstrip('\n') for line in contents]
+    model = VGG16(weights='imagenet', include_top=False)
+    feature_map = []
 
-#features = pd.DataFrame(features)
-print(features[1:5])
+    for img_path in contents:
+        features = extract_features_with_vgg16(model, img_path)
+        feature_map.append(features)
+
+    feature_map = np.matrix(feature_map)
+    np.savetxt("test.csv", feature_map, delimiter=',')
+
+if __name__ == '__main__':
+    main()
