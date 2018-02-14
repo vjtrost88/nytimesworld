11420aa255c9217e7a5e51b6f2348792b5d0afe7
Vince Trost <vincentjosephtrost@gmail.com>
2018-02-14 03:20:51 -0500

    update

--- dev/vgg16.py
+++ dev/vgg16.py
@@ -2,16 +2,21 @@ from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
+import os
 
-target_dir = "../../work/NYTimesWorld/"
+directory_in_str = "../../work/NYTImesWorld/"
+
+directory = os.fsencode(directory_in_str)
 
 model = VGG16(weights='imagenet', include_top=False)
 
 features = []
 
-for img_path in images:
-    img = image.load_img(img_path, target_size=(2048, 1365))
-    x = image.img_to_array(img)
+for img in os.listdir(directory):
+    img_name = os.fsdecode(img)
+    img_path = os.path.join(directory, img_name)
+    pic = image.load_img(img_path, target_size=(2048, 1365))
+    x = image.img_to_array(pic)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
