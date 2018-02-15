af8b2b80c9c552d1065e88adf622b6e2b383f0a4
vjtrost88 <vincent.trost@yahoo.com>
2018-02-15 01:14:27 -0500

    update

--- dev/vgg16.py
+++ dev/vgg16.py
@@ -2,10 +2,10 @@ import numpy as np
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
 
-
-def extract_features_with_vgg16(model, img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
+def extract_features(model, img_path):
+    img = image.load_img(img_path, target_size = (224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
@@ -13,18 +13,20 @@ def extract_features_with_vgg16(model, img_path):
     return features
 
 def main():
-    f = open("file-paths-r10.txt", "r")
+    f = open("file-paths.txt", "r")
     contents = f.readlines()
     contents = [line.rstrip('\n') for line in contents]
-    model = VGG16(weights='imagenet', include_top=False)
+    model = VGG16(weights='imagenet')
+    intermediate_model = Model(input=model.input, output=model.get_layer('fc2').output) 
     feature_map = []
 
     for img_path in contents:
-        features = extract_features_with_vgg16(model, img_path)
+        print("Processing {}...".format(img_path))
+        features = extract_features(intermediate_model, img_path)
         feature_map.append(features)
 
     feature_map = np.matrix(feature_map)
-    np.savetxt("test.csv", feature_map, delimiter=',')
+    np.savetxt("../../work/vgg16_feature_map.csv", feature_map, delimiter=',')
 
 if __name__ == '__main__':
     main()
